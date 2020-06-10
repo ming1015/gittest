b79723d25dc2e5be1fb4607e9af5445c9d2dc5a4
mingge <2095479021@qq.com>
2020-06-10 13:36:35 +0800

    222

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -1,10 +1,12 @@
 package com.itheima.git;
 
 public class FirstClass {
+
     public static void main(String[] args){
         System.out.println("11111");
         System.out.println("11111");
         System.out.println("11111");
         System.out.println("11111");
+        System.out.println("22222");
     }
 }
